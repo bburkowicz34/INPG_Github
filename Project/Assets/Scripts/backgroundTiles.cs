75bcb379439667cdd3ed3464824a0d7c2143e2ef
Andrzej <abaraniak.inpg@gmail.com>
2018-06-05 22:23:11 +0200

    Trochę sporo zmian
    Dodałem tła do wszystkich 3 poziomów (w 1 i 2 trzeba kopiować i przesuwać, w 3 się samo generuje, trzeba tylko podać rozmiar i skąd ma zacząć). Poza tym poprawiłem warstwy oraz 3 poziom zrobiony przez Kamila (bo był zabałaganiony). Dodałem też animowaną pochodnię do 3 poziomu. I to tyle, chyba że o czymś zapomniałem

---
+++ Project/Assets/Scripts/backgroundTiles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundTiles : MonoBehaviour {
+
+    public GameObject[] tile;
+
+    public Vector2 tileStartPos;
+    Vector2 tileSpacing;
+    public int gridWidth;
+    public int gridHeight;
+
+	// Use this for initialization
+	void Start () {
+
+        tileSpacing = tile[0].GetComponent<Renderer>().bounds.size;
+
+        for(int i=0; i<gridHeight;i++)
+        {
+            for (int j = 0; j<gridWidth; j++)
+            {
+                GameObject go = Instantiate(tile[0], new Vector3(tileStartPos.x + (j * tileSpacing.x), tileStartPos.y + (i * tileSpacing.y)), Quaternion.identity) as GameObject;
+
+                go.transform.parent = GameObject.Find("Tiles").transform;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+}
