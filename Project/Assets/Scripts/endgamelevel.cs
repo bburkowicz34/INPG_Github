a0b3d3ece5ee8819207f8265a9a0813e930327fe
Bartek <bburkowicz34@gmail.com>
2018-06-10 21:51:03 +0200

    teleport końca gry, skarb na koniec
    dodany element przenoszący gracza z 3 lvlu do main menu (jak przejdzie całą grę koło się zamyka. Na końcu jest skarb z napisem end, trzeba wpaść w ten przesmyk w lawie

---
+++ Project/Assets/Scripts/endgamelevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endgamelevel : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            SceneManager.LoadScene(0);
+    }
+}
